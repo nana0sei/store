7f78a42b5cec2d5f6a7d8872da19c06f359dae22
nana0sei <frimpongnanao@gmail.com>
2026-02-14 21:16:47 +0000

    configuring beans with annotations

--- src/main/java/com/nana0sei/store/OrderService.java
+++ src/main/java/com/nana0sei/store/OrderService.java
@@ -1,5 +1,10 @@
 package com.nana0sei.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
