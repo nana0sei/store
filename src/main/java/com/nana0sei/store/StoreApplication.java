3a10b38eec67e3c56631f1b2e64b726c71eabe34
nana0sei <frimpongnanao@gmail.com>
2026-03-01 15:51:37 +0000

    spring data jpa setup

--- src/main/java/com/nana0sei/store/StoreApplication.java
+++ src/main/java/com/nana0sei/store/StoreApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
-      var context =  SpringApplication.run(StoreApplication.class, args);
-      var userService = context.getBean(UserService.class);
-      userService.registerUser(new User(1L, "nana@mail.com", "1234", "nana osei"));
-      userService.registerUser(new User(1L, "nana@mail.com", "1234", "nana osei"));
-
+    SpringApplication.run(StoreApplication.class, args);
     }
 
 }
